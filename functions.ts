b513dd8f9e51cefe91b3566cff0c9e79ef7afe7b
hoangpc <phan.chinh.hoang@sun-asterisk.com>
2025-10-07 22:11:28 +0700

    Update

--- functions.ts
+++ functions.ts
@@ -5,11 +5,6 @@ const multiply = (a: number, b: number): number => a * b;
 function greet(name: string, role: string = "Guest"): void {
   console.log(`Hello ${name}, your role is ${role}`);
 }
-async function delayPrint(msg: string, time: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(msg);
-      resolve();
-    }, time);
-  });
-}
+console.log("Tong:", sum(1, 2));
+console.log("Tich:",multiply(3, 4))
+greet("Hoang");
