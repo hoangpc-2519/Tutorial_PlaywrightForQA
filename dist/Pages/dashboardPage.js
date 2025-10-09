40f517cef8ea1cc5dce2f72d8f7962cd044e126c
hoangpc <phan.chinh.hoang@sun-asterisk.com>
2025-10-10 01:23:06 +0700

    update

--- dist/Pages/dashboardPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DashboardPage = void 0;
-class DashboardPage {
-    async verifyLoginSuccess() {
-        console.log("Login success");
-    }
-}
-exports.DashboardPage = DashboardPage;
